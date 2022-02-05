10e7652ed064d163cad0c2ed2c45add3940ec656
Matheus Kaffka <matheus.kaffka@gmail.com>
2022-02-05 10:52:07 -0300

    config: removido timestamps da tabela Users

--- models/user.js
+++ models/user.js
@@ -1,13 +1,15 @@
 const User = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
+  const user = sequelize.define('User', {
     id: DataTypes.INTEGER,
     displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     image: DataTypes.STRING,
+  }, {
+    timestamps: false,
   });
 
-  return User;
+  return user;
 };
 
 module.exports = User;
